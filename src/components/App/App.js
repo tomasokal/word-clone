e24725845849860df69ae9b46ff75d53b9612a30
Tomas Okal <tomas.okal@gmail.com>
2023-02-03 18:48:57 -0600

    Complete exercise 1.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,18 @@
 import Game from '../Game';
 import Header from '../Header';
+import GuessInput from '../GuessInput';
 
 function App() {
   return (
     <div className="wrapper">
-      <Header />
 
+      <Header />
+      
       <div className="game-wrapper">
         <Game />
+        <GuessInput />
       </div>
+
     </div>
   );
 }
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function GuessInput() {
+
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(event) {
+
+    event.preventDefault();
+
+    if(guess.length===5){
+      console.log(guess);
+      setGuess('');
+    }
+
+  }
+
+  return (
+
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input 
+          id="guess-input" 
+          type="text" 
+          required={true}
+          autoComplete="off"
+          minLength={5}
+          maxLength={5}
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        />
+      </form>
+
+  );
+
+}
+
+export default GuessInput;
